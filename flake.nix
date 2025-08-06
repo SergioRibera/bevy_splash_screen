4b2a7cd8d084fb850390c45ab7e37b537c7d91f0
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-08-06 14:26:00 -0400

    fix: update flake thinks

--- flake.nix
+++ flake.nix
@@ -2,14 +2,12 @@
   description = "SplashScreen Bevy lib";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    cranix.url = "github:Lemin-n/cranix";
     crane.url = "github:ipetkov/crane";
     fenix.url = "github:nix-community/fenix";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
-    self,
     nixpkgs,
     flake-utils,
     ...
@@ -17,11 +15,10 @@
   # Iterate over Arm, x86 for MacOs 🍎 and Linux 🐧
     flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems) (
       system: let
+        pkgs = nixpkgs.legacyPackages.${system};
         bevyLibBundle = import ./. {
-          inherit system;
-          pkgs = nixpkgs.legacyPackages.${system};
-          crane = inputs.crane.lib;
-          cranix = inputs.cranix.lib;
+          inherit system pkgs flake-utils;
+          crane = inputs.crane.mkLib pkgs;
           fenix = inputs.fenix.packages;
         };
       in {
